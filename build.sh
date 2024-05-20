c6de649d71ae58da8694cd1e0a88e5ecc2b0bd08
osernam <osernam@gmail.com>
2024-05-19 21:10:47 -0500

    render 3

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+#poetry install
 pip install -r requirements.txt
 
-# Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
 python manage.py migrate
